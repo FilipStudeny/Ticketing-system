8a9ff0acc617c6fa0dc188e1f4bcc5386f5c739e
Filip Studený <BluesyPompanno@seznam.cz>
2023-10-04 17:37:41 +0200

    .

---
+++ Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Models;
+
+namespace TicketingSystem.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        var adminUser = new User
+        {
+            ID = "0000000FFFFF",
+            Email = "admin@example.com",
+            Firstname = "Admin",
+            Lastname = "User",
+            RegisterDate = DateTime.Now,
+            UserRole = "Administrator",
+            Password = "123456789"
+        };
+        
+        modelBuilder.Entity<User>().HasData(adminUser);
+    }
+
+}
